bbf30d1249099eeec2c56f3aa47b34756ef75192
codedeki <serbian_5@hotmail.com>
2020-04-11 11:21:50 -0400

    Card and CardList

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Card = ({ name, email, id }) => {
+    // const { name, email, id } = props; //or cleaner to put directly in paramaters
+    return (
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img src={`https://robohash.org/${id}?200x200`} alt='robots' />
+                <div>
+                    <h2>{name}</h2> 
+                    <p>{email}</p>
+                    {/* js expressions marked in curly braces */}
+                </div>
+        </div>
+    );
+}
+
+export default Card;
