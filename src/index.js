83657383ab69917c6dd99e50ce61d02a482a56d8
codedeki <serbian_5@hotmail.com>
2020-04-11 13:34:11 -0400

    Searchfield

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
-import { robots } from './robots';
 
 ReactDOM.render(
   <React.StrictMode>
     <div>
-      <CardList robots={robots} />
+      <App />
     </div>
   </React.StrictMode>,
   document.getElementById('root')
