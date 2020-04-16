35c1718a2109e753d626616ceb11040557a03a1e
codedeki <serbian_5@hotmail.com>
2020-04-16 17:35:52 -0400

    Add Redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider, Connect } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+import { searchRobots } from './reducers';
 import 'tachyons';
+
+const store = createStore(searchRobots);
+
 
 ReactDOM.render(
   <React.StrictMode>
     <div>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </div>
   </React.StrictMode>,
   document.getElementById('root')
